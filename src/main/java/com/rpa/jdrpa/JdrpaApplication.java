42eb90321ad643685617a8a1099640980634e947
JohnAndersonDuarte <johnamduarte@gmail.com>
2021-10-12 17:35:43 -0300

    Versão inicial

---
+++ src/main/java/com/rpa/jdrpa/JdrpaApplication.java
@@ -0,0 +1,13 @@
+package com.rpa.jdrpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdrpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdrpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpa/jdrpa/JdrpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpa.jdrpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdrpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
